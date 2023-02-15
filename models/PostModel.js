8e83bc44f7d2d1be56ab7a6fd8f71efaedd45825
Israel__david <kidoski1@gmail.com>
2023-02-15 15:57:41 +0000

    Update 4 files

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    author: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    content: {
+      type: Object,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+    },
   },
-  author: {
-    type: String
-  },
-  image: {
-    type: String
-  },
-  content: {
-    type: String
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'categories'
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
-module.exports = mongoose.model("posts", postSchema)
+);
+module.exports = mongoose.model("posts", postSchema);

6ceef27588699ee6006e998c48637854eba81c7a
Israel__david <kidoski1@gmail.com>
2023-03-08 17:12:02 +0000

    Update 4 files

--- models/bannerSliderModel.js
+++ models/bannerSliderModel.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const bannerSliderSchema = new mongoose.Schema({
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "posts"
-    }]
-}, {
-    timestamps: true
-})
+const bannerSliderSchema = new mongoose.Schema(
+  {
+    posts: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "posts",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("bannerSlider", bannerSliderSchema)
+module.exports = mongoose.model("bannerSlider", bannerSliderSchema);
